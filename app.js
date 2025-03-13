d1335b2b8ecd0b8e00cfa52108898ae4b26454e8
Oscar Abad <oscarabad676@gmail.com>
2025-03-13 00:46:56 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Boton agregarAmigo()
 
-let amigosLista=[];
-
-
+let amigos=[];
+muestraListaAmigos();
 function agregarAmigo(){
+
     let amigoAgregado = document.getElementById('amigo').value;
-    amigosLista.push(amigoAgregado);
-    console.log(amigosLista);
+        if (amigoAgregado ===''){
+            alert("Por favor, inserte un nombre.");
+        }
+        else {
+            amigos.push(amigoAgregado);
+            muestraListaAmigos();
+            document.querySelector('#amigo').value = '';
+            console.log(amigos);
+        }
+
+
+}
+
+function sortearAmigo(){
+    actualizaLista();
+}
+function muestraListaAmigos(){
+    let lista =document.getElementById('listaAmigos');
+    lista.innerHTML="";
+    console.log(lista);    
+    for(let i=0; i < amigos.length;i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function actualizaLista(){
+    document.querySelector('#amigo').value = '';
+    amigos = [];
+    console.log(amigos);
 }
